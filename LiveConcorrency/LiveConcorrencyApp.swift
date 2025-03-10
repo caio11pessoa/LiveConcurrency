9cb362fce7e40921cb32c1c35d5c410ee98b733b
Caio Pessoa <caiopessoa1298@gmail.com>
2025-03-10 14:08:03 -0300

    Initial Commit

---
+++ LiveConcorrency/LiveConcorrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveConcorrencyApp.swift
+//  LiveConcorrency
+//
+//  Created by Caio de Almeida Pessoa on 10/03/25.
+//
+
+import SwiftUI
+
+@main
+struct LiveConcorrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
